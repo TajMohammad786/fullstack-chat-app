2834c6964d161dc978e59932a5feecf084d3e7be
Taj-MohammadMoh <15214006-Taj-MohammadMoh@users.noreply.replit.com>
2025-02-12 05:15:45 +0000

    User checkpoint: Fix: Correct baseURL in axios instance for development environment.

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,12 +1,12 @@
-
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" 
-    ? "https://b3f8f773-e70a-444e-b53a-639ec87ca0c9-00-2omyig557gis6.spock.replit.dev/api" 
-    : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "https://b3f8f773-e70a-444e-b53a-639ec87ca0c9-00-2omyig557gis6.spock.replit.dev:8000/api"
+      : "/api",
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
